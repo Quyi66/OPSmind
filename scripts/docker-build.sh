aa8c33b61e2807dc10a9ba243955aa0d239f88fd
Jason <jason88.guo@gmail.com>
2025-09-08 09:48:27 +0800

    update

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,7 +8,7 @@ TAG_SET="false"
 
 # Release version: set here; do not pass via args. Example: 1.2.3
 # When empty, falls back to 'latest'.
-VERSION="1.0.5"
+VERSION="1.1.0"
 
 PLATFORM=""
 NO_CACHE=""
