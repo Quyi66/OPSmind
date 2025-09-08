a05b9a519cb5d7a57e3e25b62d9ce717da228cde
Jason <jason88.guo@gmail.com>
2025-09-08 08:59:14 +0800

    update

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -2,4 +2,4 @@
 
 // Centralized defaults for embed init (dev fallbacks)
 window.__OPS_RUNTIME__.DIFY_TOKEN = window.__OPS_RUNTIME__.DIFY_TOKEN || "CqLqxTQL8FeNqAqK";
-window.__OPS_RUNTIME__.DIFY_BASE_URL = window.__OPS_RUNTIME__.DIFY_BASE_URL || "http://10.1.8.229:18081";
+window.__OPS_RUNTIME__.DIFY_BASE_URL = window.__OPS_RUNTIME__.DIFY_BASE_URL || "http://10.1.40.228:18081";
