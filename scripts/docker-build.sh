72a0c4c0e44c29928b21543c5d36a3a055ef3c36
Jason <jason88.guo@gmail.com>
2025-09-16 08:46:42 +0800

    update oplus-web

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,7 +8,7 @@ TAG_SET="false"
 
 # Release version: set here; do not pass via args. Example: 1.2.3
 # When empty, falls back to 'latest'.
-VERSION="1.1.3"
+VERSION="1.1.4"
 
 PLATFORM=""
 NO_CACHE=""
